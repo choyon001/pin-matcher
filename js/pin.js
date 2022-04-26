acddd1be357545a8a3609f1ea9dbe6b681a91c86
choyon001 <dewanmonjur005@gmail.com>
2022-04-26 23:35:40 +0600

    user given pin input button created

--- js/pin.js
+++ js/pin.js
@@ -12,3 +12,26 @@ document.getElementById('generate-pin').addEventListener('click',function(){
     const randomPinInput = document.getElementById('generatePinInput');
     randomPinInput.value = generatePin();
 });
+
+// ** pad numbers value 
+
+document.getElementById('padNumbers').addEventListener('click',function(event){
+const key = event.target.innerText;
+const InputPin = document.getElementById('inputPin');
+const previousPin = InputPin.value;
+if(isNaN(key)){
+    if(key=="C"){
+        InputPin.value = '';
+    }
+    else if(key == '<'){
+        const backPin = previousPin.slice(0,previousPin.length-1);
+        InputPin.value = backPin;
+    }
+}
+else{
+    // const previousPin = InputPin.value;
+    const newPin = previousPin + key;
+    InputPin.value = newPin;
+}
+
+})
