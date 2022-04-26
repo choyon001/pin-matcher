edc8453f90823b10be66a0019142eb8e7be8a808
choyon001 <dewanmonjur005@gmail.com>
2022-04-26 22:59:04 +0600

    generate pin input added

--- js/pin.js
+++ js/pin.js
@@ -1,1 +1,14 @@
-console.log('checked');
+// generate-pin function 
+function generatePin(){
+    const pin = (Math.floor(Math.random()*10000));
+    const pinStr = pin+'';
+    if(pinStr.length<4){ 
+        return generatePin();
+    }
+    return pin;
+}
+
+document.getElementById('generate-pin').addEventListener('click',function(){
+    const randomPinInput = document.getElementById('generatePinInput');
+    randomPinInput.value = generatePin();
+});
